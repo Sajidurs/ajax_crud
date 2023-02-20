c6f434a74be5163070f135c0c5698f56a94018ae
Sajidur Rahman <shajidur171@gmail.com>
2023-02-20 15:45:50 +0600

    Not working

--- backend1.php
+++ backend1.php
@@ -1,13 +1,16 @@
 <?php
 // Database connection
 $conn = mysqli_connect("localhost","root", "", "acrud");
-if ($conn->connect_error) {
-    die("Error connecting to the server");
-} else{
-    echo "Database connection established";
-}
-
-
-// if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['mobile']) ){
-
+// if ($conn->connect_error) {
+//     die("Error connecting to the server");
 // }
+
+extract($_POST);
+
+
+
+
+if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['mobile']) ){
+    $query = " INSERT INTO `crudtable` (`firstname`, `lastname`, `email`, `mobile`) VALUES ( '$firstname', '$lastname', '$email', '$mobile' ) ";
+    mysqli_query($conn, $query);
+}
